7f7982b10a89bd14253771045010bc2388ae2963
Stoui <stouiemail@gmail.com>
2024-10-07 20:32:46 -0400

    Target dummy scene

---
+++ turbo-jam/Assets/TargetDummy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetDummy : Entity
+{   
+    public override void getHit(int Damage, Vector2 Direction)
+    {
+    }
+
+    public override void hit()
+    {
+        return;
+    }
+}
